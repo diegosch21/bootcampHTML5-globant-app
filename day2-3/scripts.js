6ef4f221331df5a9b7670c922a5e18c60147b958
Diego Schwindt <diego.sch21@gmail.com>
2012-03-14 01:28:13 -0300

    Day 2, hasta pto 4

---
+++ day2-3/scripts.js
@@ -0,0 +1,40 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+   alert("Sitio cargado");
+   $("#alias").focus();
+});
+
+$("#alias").keypress(function(event) {
+    if (event.which == 13) {
+        $("#btn").click();
+    }
+});
+
+$("#btn").click(function(event) {
+    event.preventDefault();
+    var param = $("#alias").val();
+    
+        
+    $.post('api/dispatcher.php',
+        {service: 'welcome.hello', params: {"name": ""+param+""}},
+        function(res) {
+            $("#result").html(res);
+            $("#result").addClass('resultado');
+            $("#result").removeClass('error');
+        });
+        
+    $("#alias").val('');
+    $("#alias").focus();
+    
+    $("#result").ajaxError(function(event, request, settings){
+        $(this).html("Error en " + settings.url + ": "+ request.responseText);
+        $(this).addClass('error');
+        $(this).removeClass('resultado');
+    });
+
+
+});
